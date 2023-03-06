83064cdc5c84af31312effacca8ca63e42daac9c
Andrey.Lutskiy <luzkiyac@mail.ru>
2023-03-06 13:26:26 +0300

    Исходный файл

---
+++ src/main/java/pro/sky/HashMapHW2/HashMapHw2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.HashMapHW2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashMapHw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashMapHw2Application.class, args);
+	}
+
+}
